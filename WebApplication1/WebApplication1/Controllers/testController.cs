a18fcc7821e99426c4c89329d0892b710b652ab3
charlie0822 <charlie80037@gmail.com>
2019-08-21 16:08:38 +0800

    WebApplication1

---
+++ WebApplication1/WebApplication1/Controllers/testController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApplication1.Controllers
+{
+    public class testController : Controller
+    {
+        // GET: test
+        public ActionResult Index()
+        {
+            ViewData["Name"] = "charlie";
+            ViewData["age"] = 22;
+            ViewData["single"] = true;
+            List<string> petlist = new List<string>();
+            petlist.Add("鸚鵡");
+            petlist.Add("貓");
+            petlist.Add("狗");
+            petlist.Add("魚");
+            ViewData.Model = petlist;
+            return View();
+        }
+        public ActionResult Passmassage()
+        {
+            TempData["errormassage"] = "發生錯誤";
+            TempData["username"] = "charlie";
+            TempData["time"] = DateTime.Now.ToLongTimeString();
+            return RedirectToAction(" Errormassage");            
+        }
+        public ActionResult Errormassage()
+        {
+           /* if(!TempData.ContainsKey("errormassage"))
+            {
+                return new EmptyResult();
+            }*/
+            TempData.Keep();
+            return View();
+        }
+    }
+}
